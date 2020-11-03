3554953d85ab37d9b8ed1e1d2d30c42ee66f065a
Mihai <moreliciouscod@gmail.com>
2020-11-03 22:56:35 +0100

    Update api.py

--- api.py
+++ api.py
@@ -7,6 +7,8 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 soundcloudUrl = "https://soundcloud.com/"
+instagramUrl = "https://instagram.com/"
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36',
     'From': 'spektrel@gmail.com'  # This is another valid field
@@ -26,9 +28,5 @@ def soundcloudGetTrack(user, trackname):
     soup = BeautifulSoup(page.content, 'html.parser')
     responseArray = json.loads(str(soup.find_all('script')[-1]).split("\"data\"")[8][1:-13])
     return responseArray[0]
-
-# @app.route('/instagram', methods=['GET'])
-# def api_all():
-#     return flask.jsonify(insta)
 
 app.run()
