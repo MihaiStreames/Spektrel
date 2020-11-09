5c9624cd6260ab9d245c4aec22b36b57ce43450e
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2020-11-09 14:45:16 +0200

    homepage frontend done

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
     <div class="home-header">
-      <a href="/"><div class="home-logo"></div></a>
+      <router-link to="/"><div class="home-logo"></div></router-link>
       <ul class="home-headerButtons">
-        <li><a href="/about">About</a></li>
+        <li><router-link to="/about">About</router-link></li>
         <li><a href="https://github.com/MihaiStreames/Spektrel" target="_blank">Repository</a></li>
-        <li><a href="/platforms">Platforms</a></li>
-        <li><a href="/dashboard" class="home-dashboardButton">Dashboard</a></li>
+        <li><router-link to="/platforms">Platforms</router-link></li>
+        <li>
+          <router-link to="/dashboard" class="home-dashboardButton">
+            Open Dashboard
+          </router-link>
+        </li>
       </ul>
     </div>
     <section class="home-firstBackground">
@@ -18,8 +22,8 @@
           posts and download csv statistics for you to use
         </p>
         <div class="home-Buttons">
-          <a href="/login" class="home-loginButton">Login</a>
-          <a href="/signup" class="home-signupButton">Signup</a>
+          <router-link to="/login" class="home-loginButton">Login</router-link>
+          <router-link to="/signup" class="home-signupButton">Signup</router-link>
         </div>
       </div>
       <vue-particles
@@ -31,6 +35,21 @@
       </vue-particles>
       <div class="home-firstSlideBackground"></div>
     </section>
+    <section class="home-secondBackground">
+      <div class="home-secondSlideContent">
+        <div class="home-dashboardScreenshot"></div>
+        <div class="home-secondSlideTitle">
+          <h1>Spektrel lets you quickly check your statistics</h1>
+          <p>
+            All your platforms show up on the main navigation letting you quickly
+            switch to them letting you check your latest statistics in
+            real time while also having notifications for each app to quickly
+            show you your growth before even having to click
+          </p>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
