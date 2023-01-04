4f81a6df312c98e316fd8b31fbe92fdc3ad66dc3
Mihai <rares.bors@ulb.be>
2023-01-04 19:36:07 +0100

    json logging
    added json logging :+1:

--- api.py
+++ api.py
@@ -20,17 +20,41 @@ headers = {
 def soundcloudGetUser(username):
     page = requests.get(soundcloudUrl + username, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
-    response = str(soup.find_all('script')).split("\"data\"")[-1][1:]  # Apparently it's always the last one
-    return json.loads(response.split(';')[0][0:-2])
 
+    # Bruteforce user data out of page
+    response = str(soup.find_all('script')).split("\"data\"")[-1][1:]  # Apparently it's always the last one
+    responseJSON = json.loads(response.split(';')[0][0:-2])
+
+    # Clean up the response to a more simple object
+    result = {
+        "Username": responseJSON["username"],
+        "Profile_Picture": responseJSON["avatar_url"],
+        "Followers": responseJSON["followers_count"],
+        "Following": responseJSON["followings_count"],
+        "Tracks": responseJSON["track_count"],
+    }
+    return result
 
 # Soundcloud Track Router
 @app.route('/soundcloud/track/<path:user>/<path:trackname>')
 def soundcloudGetTrack(user, trackname):
     page = requests.get(soundcloudUrl + user + "/" + trackname, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Bruteforce track data out of page
     response = str(soup.find_all('script')).split("\"data\"")[-1][1:]
-    return json.loads(response.split('media')[0][0:-2] + '}')
+    responseJSON = json.loads(response.split('media')[0][0:-2] + '}') # Same here but weirder
+
+    result = {
+        "Plays": responseJSON["playback_count"],
+        "Likes": responseJSON["likes_count"],
+        "Comments": responseJSON["comment_count"],
+        "Reposts": responseJSON["reposts_count"],
+        "Downloads": responseJSON["download_count"],
+        "Cover": responseJSON["artwork_url"],
+        "Title": responseJSON["title"],
+    }
+    return result
 
 
 app.run()
